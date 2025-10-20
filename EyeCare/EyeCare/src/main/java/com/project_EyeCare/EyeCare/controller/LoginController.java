22c3a1f9310d018836d418d5b4672d4ce7f5eb55
DESKTOP-BBLDESS\leeyw <hwa8078678@gmail.com>
2025-10-21 01:03:59 +0900

    되돌리기

--- EyeCare/EyeCare/src/main/java/com/project_EyeCare/EyeCare/controller/LoginController.java
+++ EyeCare/EyeCare/src/main/java/com/project_EyeCare/EyeCare/controller/LoginController.java
@@ -15,4 +15,31 @@ public class LoginController {
     public String register(){
         return "/account/register.html";
     }
+<<<<<<< HEAD
 }
+=======
+
+    @PostMapping("/member")
+    public String addMember(@RequestParam("name") String userName,
+                          @RequestParam("email") String email,
+                          @RequestParam("password") String password,
+                          @RequestParam("phone") String phone,
+                          @RequestParam("address") String address,
+                          @RequestParam("visionLeft") float visionLeft,
+                          @RequestParam("visionRight") float visionRight) {
+        
+        Member member = new Member();
+        member.setUser_name(userName);
+        member.setEmail(email);
+        member.setPassword(password);
+        member.setPhone(phone);
+        member.setAddress(address);
+        member.setVision_left(visionLeft);
+        member.setVision_right(visionRight);
+
+        memberRepository.save(member);
+        
+        return "redirect:/login";  // 회원가입 후 로그인 페이지로 리다이렉트
+    }
+}
+>>>>>>> parent of f06d52f (체크)
